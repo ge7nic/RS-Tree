9c9d0787a2d5e88284fa6203da3e4019a996b866
ge7nic <48736317+ge7nic@users.noreply.github.com>
2020-12-28 02:21:47 +0100

    added JavaFX for graphical overlay

--- src/main/java/de/getto/nicolas/App.java
+++ src/main/java/de/getto/nicolas/App.java
@@ -2,22 +2,34 @@ package de.getto.nicolas;
 
 import de.getto.nicolas.node.RBNode;
 import de.getto.nicolas.tree.RedBlackTree;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
 
+	@Override
+	public void start(Stage stage) {
+		tryoutProgram(stage);
+	}
+	
 	public static void main(String[] args) {
-		RedBlackTree<Integer> rbTree = new RedBlackTree<>();
-		RBNode<Integer> x = new RBNode<Integer>(5);
-		RBNode<Integer> y = new RBNode<Integer>(11);
-		RBNode<Integer> z = new RBNode<Integer>(3);
-		RBNode<Integer> temp = new RBNode<Integer>(7);
-		rbTree.insertNodeBU(x);
-		rbTree.insertNodeBU(y);
-		rbTree.insertNodeBU(z);
-		rbTree.insertNodeBU(temp);
-		System.out.println(rbTree.sideViewPrint());
-		System.out.println(rbTree.deleteRBNode(x));
-		System.out.println(rbTree.sideViewPrint());
+		launch();
+	}
+	
+	private void sampleProgram(Stage stage) {
+		String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+	}
+	
+	private void tryoutProgram(Stage stage) {
+		
 	}
 
 }
