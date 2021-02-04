ee989dfcc4925b0ffeacfca5c3d75728c91dfcac
ge7nic <48736317+ge7nic@users.noreply.github.com>
2021-02-04 22:31:05 +0100

    Made POM changes to build shaded .jar

--- src/main/java/de/getto/nicolas/App.java
+++ src/main/java/de/getto/nicolas/App.java
@@ -38,7 +38,6 @@ public class App extends Application {
 	} 
 	
 	public static void main(String[] args) {
-		launch(args);
+		App.launch(args);
 	}
-	
 }
---
+++ src/main/java/de/getto/nicolas/AppStarter.java
@@ -0,0 +1,9 @@
+package de.getto.nicolas;
+
+public class AppStarter {
+
+	public static void main(final String[] args) {
+		App.main(args);
+	}
+
+}
